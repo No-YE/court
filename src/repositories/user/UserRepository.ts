efd2e9737148b14519b0f6244f7faa3da47dd1df
No-YE <nye7181@gmail.com>
2019-08-27 15:32:03 +0900

    userrepository 인터페이스에 findPoints 추가

--- src/repositories/user/UserRepository.ts
+++ src/repositories/user/UserRepository.ts
@@ -7,4 +7,5 @@ export interface UserRepository {
   updatePoint(id: ObjectId, point: number): Promise<UpdateWriteOpResult>;
   delete(id: ObjectId): Promise<DeleteWriteOpResultObject>;
   isAdmin(id: ObjectId): Promise<boolean>;
+  findPoints(id: ObjectId, offset: number, limit: number): Promise<ObjectId[]>;
 }
