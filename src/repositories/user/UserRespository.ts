fe4297e2d36d2c40c5b493a2856fc55b5a624f58
No-YE <nye7181@gmail.com>
2019-08-27 15:18:47 +0900

    UserRepository 인터페이스 작성

---
+++ src/repositories/user/UserRespository.ts
@@ -0,0 +1,10 @@
+import { InsertOneWriteOpResult, ObjectId, DeleteWriteOpResultObject, UpdateWriteOpResult } from 'mongodb';
+import { User } from '../../entities';
+
+export interface UserRepository {
+  create(user: User): Promise<InsertOneWriteOpResult>;
+  updatePresentation(id: ObjectId, num: number): Promise<UpdateWriteOpResult>;
+  updatePoint(id: ObjectId, point: number): Promise<UpdateWriteOpResult>;
+  delete(id: ObjectId): Promise<DeleteWriteOpResultObject>;
+  isAdmin(id: ObjectId): Promise<boolean>;
+}
